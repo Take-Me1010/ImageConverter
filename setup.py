a0db6bc995573d5dc8b50b0eecceaee5fd74e7bc
Take-Me1010 <take_ohmi@icloud.com>
2022-02-15 20:29:23 +0900

    setup.pyを追加

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+def _requires_from_file(filename):
+    return open(filename, encoding="utf-8").read().splitlines()
+
+
+setup(
+    name='imgconv',
+    version='0.1.0',
+    install_requires=_requires_from_file("requirements.txt"),
+    packages=["dist"],
+    entry_points={
+        'console_scripts': [
+            "imgconv=dist.main:main"
+        ]
+    }
+)
