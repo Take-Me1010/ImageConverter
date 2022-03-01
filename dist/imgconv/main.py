d6bb67f4d014c686bf6c9b4cb6962ceee86377b5
Take-Me1010 <take_ohmi@icloud.com>
2022-03-01 22:41:50 +0900

    build for iconextractor

--- dist/imgconv/main.py
+++ dist/imgconv/main.py
@@ -1,8 +1,11 @@
-import argparse
 import pdf2image
+import argparse
 import logging
-from PIL import ImageDraw, ImageFilter, UnidentifiedImageError, Image
-from typing import Dict, NamedTuple, Union, Any, Optional, List
+import pefile
+import struct
+import io
+from typing import Optional, Any, Dict, NamedTuple, Union, List
+from PIL import ImageDraw, UnidentifiedImageError, Image, ImageFilter
 from pathlib import Path
 
 
@@ -126,6 +129,185 @@ def parse(*args, **kwargs) -> Args:
     namespace: Args = parser.parse_args(*args, **kwargs)        # type: ignore
 
     return namespace
+"""
+Windows PE EXE icon extractor.
+TODO: resolve linting error
+
+The MIT License (MIT)
+Copyright (c) 2022 Take-Me1010
+
+The class IconExtractor is based on https://github.com/jlu5/icoextract .
+I change logger (before: global variable) into its class attribute.
+I use this module to implement the feature to extract an ico file from an exe file.
+
+Copyright (c) 2015-2016 Fadhil Mandaga
+Copyright (c) 2019 James Lu <james@overdrivenetworks.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+GRPICONDIRENTRY_FORMAT = ('GRPICONDIRENTRY',
+                          ('B,Width', 'B,Height', 'B,ColorCount', 'B,Reserved',
+                           'H,Planes', 'H,BitCount', 'I,BytesInRes', 'H,ID'))
+GRPICONDIR_FORMAT = ('GRPICONDIR', ('H,Reserved', 'H,Type', 'H,Count'))
+
+
+class IconExtractorError(Exception):
+    pass
+
+
+class NoIconsAvailableError(IconExtractorError):
+    pass
+
+
+class InvalidIconDefinitionError(IconExtractorError):
+    pass
+
+
+class IconExtractor():
+    """IconExtractor from exe file.
+
+    This class is fully based on great repo https://github.com/jlu5/icoextract
+
+    """
+
+    def __init__(self, filename: str, logger: logging.Logger):
+        self.filename = filename
+        self.logger = logger
+        # Use fast loading and explicitly load the RESOURCE directory entry. This saves a LOT of time
+        # on larger files
+        self._pe = pefile.PE(filename, fast_load=True)
+        self._pe.parse_data_directories(pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_RESOURCE'])
+
+        if not hasattr(self._pe, 'DIRECTORY_ENTRY_RESOURCE'):
+            raise NoIconsAvailableError(f"{filename} has no resources")
+
+        # Reverse the list of entries before making the mapping so that earlier values take precedence
+        # When an executable includes multiple icon resources, we should use only the first one.
+        resources = {rsrc.id: rsrc for rsrc in reversed(self._pe.DIRECTORY_ENTRY_RESOURCE.entries)}
+
+        self.groupiconres = resources.get(pefile.RESOURCE_TYPE["RT_GROUP_ICON"])
+        if not self.groupiconres:
+            raise NoIconsAvailableError(f"{filename} has no group icon resources")
+        self.rticonres = resources.get(pefile.RESOURCE_TYPE["RT_ICON"])
+
+    def list_group_icons(self):
+        """
+        Returns a list of group icon entries.
+        """
+        return [(e.struct.Name, e.struct.OffsetToData)
+                for e in self.groupiconres.directory.entries]
+
+    def _get_group_icon_entries(self, num=0):
+        """
+        Returns the group icon entries for the specified group icon in the executable.
+        """
+        groupicon = self.groupiconres.directory.entries[num]
+        if groupicon.struct.DataIsDirectory:
+            # Select the first language from subfolders as needed.
+            groupicon = groupicon.directory.entries[0]
+
+        # Read the data pointed to by the group icon directory (GRPICONDIR) struct.
+        rva = groupicon.data.struct.OffsetToData
+        size = groupicon.data.struct.Size
+        data = self._pe.get_data(rva, size)
+        file_offset = self._pe.get_offset_from_rva(rva)
+
+        grp_icon_dir = self._pe.__unpack_data__(GRPICONDIR_FORMAT, data, file_offset)
+        self.logger.debug(grp_icon_dir)
+
+        if grp_icon_dir.Reserved:
+            raise InvalidIconDefinitionError(
+                "Invalid group icon definition (got Reserved=%s instead of 0)" % hex(
+                    grp_icon_dir.Reserved))
+
+        # For each group icon entry (GRPICONDIRENTRY) that immediately follows, read its data and save it.
+        grp_icons = []
+        icon_offset = grp_icon_dir.sizeof()
+        for idx in range(grp_icon_dir.Count):
+            grp_icon = self._pe.__unpack_data__(GRPICONDIRENTRY_FORMAT, data[icon_offset:], file_offset + icon_offset)
+            icon_offset += grp_icon.sizeof()
+            grp_icons.append(grp_icon)
+            self.logger.debug("Got logical group icon %s", grp_icon)
+
+        return grp_icons
+
+    def _get_icon_data(self, icon_ids):
+        """
+        Return a list of raw icon images corresponding to the icon IDs given.
+        """
+        icons = []
+        icon_entry_lists = {icon_entry_list.id: icon_entry_list for icon_entry_list in self.rticonres.directory.entries}
+        for icon_id in icon_ids:
+            icon_entry_list = icon_entry_lists[icon_id]
+
+            icon_entry = icon_entry_list.directory.entries[0]  # Select first language
+            rva = icon_entry.data.struct.OffsetToData
+            size = icon_entry.data.struct.Size
+            data = self._pe.get_data(rva, size)
+            self.logger.debug(f"Exported icon with ID {icon_entry_list.id}: {icon_entry.struct}")
+            icons.append(data)
+        return icons
+
+    def _write_ico(self, fd, num=0):
+        """
+        Writes ICO data to a file descriptor.
+        """
+        group_icons = self._get_group_icon_entries(num=num)
+        icon_images = self._get_icon_data([g.ID for g in group_icons])
+        icons = list(zip(group_icons, icon_images))
+        assert len(group_icons) == len(icon_images)
+        fd.write(b"\x00\x00")  # 2 reserved bytes
+        fd.write(struct.pack("<H", 1))  # 0x1 (little endian) specifying that this is an .ICO image
+        fd.write(struct.pack("<H", len(icons)))  # number of images
+
+        dataoffset = 6 + (len(icons) * 16)
+        # First pass: write the icon dir entries
+        for datapair in icons:
+            group_icon, icon_data = datapair
+            # Elements in ICONDIRENTRY and GRPICONDIRENTRY are all the same
+            # except the last value, which is an ID in GRPICONDIRENTRY and
+            # the offset from the beginning of the file in ICONDIRENTRY.
+            fd.write(group_icon.__pack__()[:12])
+            fd.write(struct.pack("<I", dataoffset))
+            dataoffset += len(icon_data)  # Increase offset for next image
+
+        # Second pass: write the icon data
+        for datapair in icons:
+            group_icon, icon_data = datapair
+            fd.write(icon_data)
+
+    def export_icon(self, fname, num=0):
+        """
+        Writes ICO data containing the program icon of the input executable.
+        """
+        with open(fname, 'wb') as f:
+            self._write_ico(f, num=num)
+
+    def get_icon(self, num=0):
+        """
+        Returns ICO data as a BytesIO() instance, containing the program icon of the input executable.
+        """
+        f = io.BytesIO()
+        self._write_ico(f, num=num)
+        return f
 """
 CLI本体を定義する。
 """
@@ -352,6 +534,28 @@ def convert(img_input: Path, img_output: Path, preprocessor: Preprocessor, pdf2i
     logger.info(f"successfully converted {img_input} into {img_output}")
 
 
+def extract_icon(img_input: Path, img_output: Path, num: int = 0):
+    """exeからiconを取り出す
+
+    Args:
+        img_input (Path): 入力ファイル(.exe)
+        img_output (Path): 出力ファイル名(.ico)
+        num (int, optional): 何番目のiconを出力するか. Defaults to 0.
+    """
+    if img_output.suffix != ".ico":
+        logger.error(f"IconExtractor do not support {img_output.suffix} now...")
+        logger.warning(f"failed to extract {img_input} into {img_output}.")
+        return
+
+    try:
+        IconExtractor(str(img_input), logger).export_icon(img_output, num)
+    except IconExtractorError as err:
+        logger.error(f"during extracting {img_input} into {img_output}, encountered an error: {err}")
+        logger.warning("failed to extract.")
+    else:
+        logger.info(f"successfully extract {img_input} into {img_output}")
+
+
 def resolve_output_file_path(img_input: Path, out: str) -> Path:
     """outの形式に応じて、出力先のパスを返す
 
@@ -376,8 +580,13 @@ def resolve_output_file_path(img_input: Path, out: str) -> Path:
 def get_img_inputs_from_user_inputs(inputs: List[str]):
     """ 入力されたファイルを順にイテレーションする """
     for pattern in inputs:
-        for img_input in Path.cwd().glob(pattern):
-            yield img_input
+        logger.debug(pattern)
+        if Path(pattern).is_absolute():
+            yield Path(pattern)
+
+        else:
+            for img_input in Path.cwd().glob(pattern):
+                yield img_input
 
 
 def main():
@@ -392,7 +601,11 @@ def main():
 
     for img_input in get_img_inputs_from_user_inputs(img_inputs):
         img_output = resolve_output_file_path(img_input, out)
-        convert(img_input, img_output, preprocessor, pdf2image_options)
+        if img_input.suffix == ".exe":
+            extract_icon(img_input, img_output)
+
+        else:
+            convert(img_input, img_output, preprocessor, pdf2image_options)
 
 
 if __name__ == '__main__':
